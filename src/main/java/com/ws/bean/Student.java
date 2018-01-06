c2b4295f75bd0f7b1f306abb0303cb3ff5ef82a9
wanshuai <771034201@qq.com>
2018-01-07 01:14:59 +0800

    测试

---
+++ src/main/java/com/ws/bean/Student.java
@@ -0,0 +1,14 @@
+package com.ws.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+	
+	private int id;
+	private String name;
+}
